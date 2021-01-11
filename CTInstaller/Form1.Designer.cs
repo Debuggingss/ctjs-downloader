82cbe7b47be5c3bc2ea154ff73b63c6e5c582ac2
Debuggingss <55459283+Debuggingss@users.noreply.github.com>
2021-01-11 11:17:29 +0100

    Add files via upload

---
+++ CTInstaller/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CTInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(15, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download Forge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(15, 83);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Download ctjs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 176);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(254, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 30);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Made by Debug#7682";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 248);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CTInstaller/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Forms;
+
+namespace CTInstaller
+{
+    public partial class Form1 : Form
+    {
+        public static string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public static string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Downloading Forge...";
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(
+                    new Uri("https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer-win.exe"),
+                    desktop + "\\FORGE_INSTALLER.exe"
+                );
+                wc.DownloadFileCompleted += wc_DownloadFinished1;
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Downloading ctjs...";
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(
+                    new Uri("https://github.com/ChatTriggers/ChatTriggers/releases/download/1.3.0/ctjs-1.3.0-1.8.9.jar"),
+                    appdata + "\\.minecraft\\mods\\ctjs-1.3.0-1.8.9.jar"
+                );
+                wc.DownloadFileCompleted += wc_DownloadFinished2;
+            }
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        void wc_DownloadFinished1(object sender, AsyncCompletedEventArgs e)
+        {
+            label1.Text = "Forge Downloaded!";
+            Process.Start(desktop + "\\FORGE_INSTALLER.exe");
+            MessageBox.Show("Click 'OK' in the Forge Installer window!");
+        }
+
+        void wc_DownloadFinished2(object sender, AsyncCompletedEventArgs e)
+        {
+            label1.Text = "ctjs Downloaded!";
+            MessageBox.Show("Now you should be ready to launch forge!");
+        }
+    }
+}
